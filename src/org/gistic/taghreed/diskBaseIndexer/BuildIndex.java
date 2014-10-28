b8dc1261ae7cd3b211856c32d78ad02751e8c122
louai <louai@louais-MacBook-Pro.local>
2014-10-27 22:56:48 -0500

    few change

--- src/org/gistic/taghreed/diskBaseIndexer/BuildIndex.java
+++ src/org/gistic/taghreed/diskBaseIndexer/BuildIndex.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
