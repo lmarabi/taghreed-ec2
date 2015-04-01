5da96b6bfd998ebc4f1a2da957e8c50a8077b8d6
Louai AlArabi <louai@alrazi.cs.umn.edu>
2015-04-01 12:49:00 -0500

    add the basic operations

--- src/umn/ec2/exp/Main.java
+++ src/umn/ec2/exp/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		if(args.length ==2){
+			String operation = args[0];
+			String level = args[1];
+		}else{
+			System.out.println("To use this program you must pass the following arguments\n*********\n"
+					+ "index [level(day,week,month)]\n"
+					+ "query [level(day,week,month)]\n"
+					+ "query temporal\t this is will query from all levels");
+		}
 	}
 
 }
