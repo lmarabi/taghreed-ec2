6cc5a514f89a1869d2c0cb4ab4aa11d30bdd429a
louai <louai@louais-MacBook-Pro.local>
2015-05-22 09:44:16 -0500

    update

--- src/umn/ec2/exp/Main.java
+++ src/umn/ec2/exp/Main.java
@@ -16,11 +16,10 @@ import com.sun.rmi.rmid.ExecPermission;
 
 public class Main {
 
-
 	public static void main(String[] args) throws Exception {
-//		args = new String[2];
-//		args[0] = "query";
-//		args[1] = "spatial";
+		// args = new String[2];
+		// args[0] = "query";
+		// args[1] = "spatial";
 		// TODO Auto-generated method stub
 		if (args.length == 2) {
 			String operation = args[0];
@@ -28,7 +27,9 @@ public class Main {
 			if (operation.equals("index")) {
 				System.out.println("Indexing operation is running Now");
 				OutputStreamWriter writer = new OutputStreamWriter(
-						new FileOutputStream(System.getProperty("user.dir") + "/"+ "IndexOperation_time.log", true), "UTF-8");
+						new FileOutputStream(System.getProperty("user.dir")
+								+ "/" + "IndexOperation_time.log", true),
+						"UTF-8");
 				MainBackendIndex indexOp = new MainBackendIndex();
 				Responder respondHandler = new Responder();
 				indexOp.setHandler(respondHandler);
@@ -36,26 +37,28 @@ public class Main {
 					indexOp.indexDayLevel();
 				} else if (level.equals("week")) {
 					indexOp.indexWeekLevel();
-				} else if(level.equals("month")){
+				} else if (level.equals("month")) {
 					indexOp.indexMonthLevel();
-				}else{
+				} else {
 					indexOp.indexWholeOneIndex();
 				}
-				System.out.println("Total Indexing Time ("+level+") ********** "+respondHandler.getTotalExecutionTimes());
-				writer.write("\n"+level+","+respondHandler.getTotalExecutionTimes());
+				System.out.println("Total Indexing Time (" + level
+						+ ") ********** "
+						+ respondHandler.getTotalExecutionTimes());
+				writer.write("\n" + level + ","
+						+ respondHandler.getTotalExecutionTimes());
 				writer.close();
 
-			} 
-		}else if (args.length == 3)
-		{
+			}
+		} else if (args.length == 3) {
 			String operation = args[0];
 			String parameter = args[1];
 			int startDay = Integer.parseInt(args[2]);
 			if (operation.equals("query")) {
 				System.out.println("Query operation is running Now");
-				RangeQueryExperiments(parameter,startDay);
+				RangeQueryExperiments(parameter, startDay);
 			}
-			
+
 		} else {
 			System.out
 					.println("To use this program you must pass the following arguments\n*********\n"
@@ -64,27 +67,29 @@ public class Main {
 							+ "query [spatial|temporal]\tstartDay - int number only for example 1 or 13");
 		}
 	}
-	
-	private static void RangeQueryExperiments(String parameter, int startDay) throws Exception{
-		if(parameter.equals("spatial")){
+
+	private static void RangeQueryExperiments(String parameter, int startDay)
+			throws Exception {
+		if (parameter.equals("spatial")) {
 			spatialRangeQueryExpr(startDay);
-		}else if(parameter.equals("temporal")){
+		} else if (parameter.equals("temporal")) {
 			temporalRangeQueryExpr(startDay);
-		}else{
-			// this change the query execution techniques. 
+		} else {
+			// this change the query execution techniques.
 		}
 	}
-	
+
 	/***
-	 * In this method we change the spatial Range of the query
-	 * And fix the following: Temporal , And Query Processing.
-	 * @throws Exception 
+	 * In this method we change the spatial Range of the query And fix the
+	 * following: Temporal , And Query Processing.
+	 * 
+	 * @throws Exception
 	 */
-	private static void spatialRangeQueryExpr(int startDay) throws Exception{
-//		double[] area = {0.0001,0.001,0.01};
-		double[] area = {0.000001,0.0001,0.01,0.1};
-		for(int i=0 ; i< area.length; i++){
-			
+	private static void spatialRangeQueryExpr(int startDay) throws Exception {
+		// double[] area = {0.0001,0.001,0.01};
+		double[] area = { 0.000001, 0.0001, 0.01, 0.1 };
+		for (int i = 0; i < area.length; i++) {
+
 			SamplersCollector sampleHandler = new SamplersCollector();
 			ServerRequest req = new ServerRequest();
 			req.setStartDate("2014-05-01");
@@ -93,96 +98,98 @@ public class Main {
 			req.setIndex(queryIndex.rtree);
 			req.setNumSamples(20);
 			/*
-			 * Read A sample from Index 
-			 * */
+			 * Read A sample from Index
+			 */
 			Queryoptimizer queryExec = new Queryoptimizer(req);
 			queryExec.setExpName("TakeSamples");
 			queryExec.addSampleHandler(sampleHandler);
 			queryExec.readSamplesMBR();
-			req.setRect(sampleHandler.getSamples(),area[i]);
+			req.setRect(sampleHandler.getSamples(), area[i]);
 			/*
 			 * Now Execute the Range Query
-			 * */
-			
+			 */
+
 			String startTime = "2014-05-01";
 			String endTime = "2014-05-";
 			queryExec = new Queryoptimizer(req);
 			queryExec.setSpatialRatio(area[i]);
 			queryExec.setExpName("RangeQueryExp_Spatial");
-			for(int d=startDay ; d<32; d++){
-				if(d < 10){
-					endTime = "2014-05-0"+d;
-				}else{
-					endTime = "2014-05-"+d;
+			for (int d = startDay; d < 32; d++) {
+				if (d == 1 || d == 3 || d == 4 || d == 10 || d == 11 || d == 17
+						|| d == 18 || d == 24 || d == 25 | d == 31) {
+					if (d < 10) {
+						endTime = "2014-05-0" + d;
+					} else {
+						endTime = "2014-05-" + d;
+					}
+					req.setStartDate(startTime);
+					req.setEndDate(endTime);
+					try {
+						queryExec.executeQuery();
+					} catch (Exception ex) {
+
+					}
+
 				}
-				req.setStartDate(startTime);
-				req.setEndDate(endTime);
-				try{
-					queryExec.executeQuery();	
-				}catch(Exception ex){
-					
-				}
-				
 			}
-			
 		}
-		
+
 	}
-	
+
 	/***
-	 * In this method we change the temporal Range of the query
-	 * And fix the following: spatial, and query processing technique
-	 * @throws Exception 
+	 * In this method we change the temporal Range of the query And fix the
+	 * following: spatial, and query processing technique
+	 * 
+	 * @throws Exception
 	 */
-	private static void temporalRangeQueryExpr(int startDay) throws Exception{
+	private static void temporalRangeQueryExpr(int startDay) throws Exception {
 		SamplersCollector sampleHandler = new SamplersCollector();
 		ServerRequest req = new ServerRequest();
 		req.setStartDate("2014-05-01");
 		req.setEndDate("2014-05-31");
 		req.setType(queryType.tweet);
 		req.setIndex(queryIndex.rtree);
-//		double maxlon = -93.18933596240234;
-//		double minlat = 44.94941027490235;
-//		double maxlat = 45.01670153466797;
-//		double minlon = -93.3176528416748;
-//		MBR mbr = new MBR(new Point(maxlat, maxlon), new Point(minlat, minlon));
-//		req.setMBR(mbr);
+		// double maxlon = -93.18933596240234;
+		// double minlat = 44.94941027490235;
+		// double maxlat = 45.01670153466797;
+		// double minlon = -93.3176528416748;
+		// MBR mbr = new MBR(new Point(maxlat, maxlon), new Point(minlat,
+		// minlon));
+		// req.setMBR(mbr);
 		req.setNumSamples(20);
 		/*
-		 * Read A sample from Index 
-		 * */
+		 * Read A sample from Index
+		 */
 		Queryoptimizer queryExec = new Queryoptimizer(req);
 		queryExec.setExpName("TakeSamples");
 		queryExec.addSampleHandler(sampleHandler);
 		queryExec.readSamplesMBR();
-		req.setRect(sampleHandler.getSamples(),(double)0.0001);
+		req.setRect(sampleHandler.getSamples(), (double) 0.0001);
 		/*
 		 * Now Execute the Range Query
-		 * */
-		
+		 */
+
 		String startTime = "2014-05-01";
 		String endTime = "2014-05-";
 		queryExec = new Queryoptimizer(req);
-		queryExec.setSpatialRatio((double)0.0001);
+		queryExec.setSpatialRatio((double) 0.0001);
 		queryExec.setExpName("RangeQueryExp_temporal");
-		for(int i=startDay ; i<32; i++){
-			if(i < 10){
-				endTime = "2014-05-0"+i;
-			}else{
-				endTime = "2014-05-"+i;
+		for (int i = startDay; i < 32; i++) {
+			if (i < 10) {
+				endTime = "2014-05-0" + i;
+			} else {
+				endTime = "2014-05-" + i;
 			}
 			req.setStartDate(startTime);
 			req.setEndDate(endTime);
-			try{
-				queryExec.executeQuery();	
-			}catch(Exception ex){
-				
+			try {
+				queryExec.executeQuery();
+			} catch (Exception ex) {
+
 			}
-			
-			
+
 		}
-		
+
 	}
-	
 
 }
